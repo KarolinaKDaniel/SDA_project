8e8fc250e92e28a7c7305a31c1d880e3b099c3c5
Uzytkownik <uztkownik@kursGIT.pl>
2021-12-15 19:05:39 +0100

    add model Alert

--- final_project/e_drugs/models.py
+++ final_project/e_drugs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models import Model, CharField, TextField
+
+
+class Alert(Model):
+    name = CharField(max_length=128)
+    recommendations = TextField()
 
 # Create your models here.
