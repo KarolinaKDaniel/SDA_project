f15e4b69fcbb3f5b33b8fec4dc368ae0f650a308
Rafal <kalawski1900@gmail.com>
2021-12-15 20:35:44 +0100

    add and registrate model Affliction

--- final_project/e_drugs/admin.py
+++ final_project/e_drugs/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Alert, Shipping, Substance
+from .models import Alert, Shipping, Substance, Affliction
 
 
 # Register your models here.
 admin.site.register(Alert)
 admin.site.register(Shipping)
 admin.site.register(Substance)
+admin.site.register(Affliction)
--- final_project/e_drugs/models.py
+++ final_project/e_drugs/models.py
@@ -17,3 +17,8 @@ class Substance(Model):
     name = CharField(max_length=20)
     is_active = BooleanField(default=True)
     do_not_use_with = ManyToManyField("Substance", related_name="forbidden")
+
+
+class Affliction(Model):
+    name = CharField(max_length=128)
+    do_not_use = ManyToManyField(Substance, related_name="forbidden_sub")
