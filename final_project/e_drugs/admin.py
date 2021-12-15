e2769f2d363e5877e16e593b11cc58075bf0f36c
Uzytkownik <uztkownik@kursGIT.pl>
2021-12-15 20:07:29 +0100

    add model Shipping

--- final_project/e_drugs/admin.py
+++ final_project/e_drugs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Alert
+from .models import Alert, Shipping
 
 
 # Register your models here.
 admin.site.register(Alert)
+admin.site.register(Shipping)
--- final_project/e_drugs/models.py
+++ final_project/e_drugs/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-from django.db.models import Model, CharField, TextField
+from django.db.models import Model, CharField, TextField, FloatField
 
 
 class Alert(Model):
     name = CharField(max_length=128)
     recommendations = TextField()
+
+
+class Shipping(Model):
+    name = CharField(max_length=128)
+    price = FloatField()
+
 
 # Create your models here.
