a399f7964d78e38b9487c785ada1144de77077a5
Rafal <kalawski1900@gmail.com>
2021-12-15 19:45:29 +0100

    fixed typo "max_length" add registration Substance's model

--- final_project/e_drugs/models.py
+++ final_project/e_drugs/models.py
@@ -1,7 +1,7 @@
 from django.db.models import Model, CharField, BooleanField, ManyToManyField
 
 class Substance(Model):
-    name = CharField(max_lenght=20)
+    name = CharField(max_length=20)
     is_active = BooleanField(default=True)
     do_not_use_with = ManyToManyField("self", related_name="forbidden")
 
