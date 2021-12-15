66604c2a234ea3248488988d32a345644811f5a7
Rafal <kalawski1900@gmail.com>
2021-12-15 19:56:15 +0100

    add and registrated model Substance

--- final_project/e_drugs/admin.py
+++ final_project/e_drugs/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Alert
+from .models import Alert, Substance
 
 
 # Register your models here.
 admin.site.register(Alert)
+admin.site.register(Substance)
--- final_project/e_drugs/models.py
+++ final_project/e_drugs/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-from django.db.models import Model, CharField, TextField
+from django.db.models import Model, CharField, TextField, BooleanField, ManyToManyField
 
 
 class Alert(Model):
     name = CharField(max_length=128)
     recommendations = TextField()
 
+
 # Create your models here.
+class Substance(Model):
+    name = CharField(max_length=20)
+    is_active = BooleanField(default=True)
+    do_not_use_with = ManyToManyField("self", related_name="forbidden")
