69d51a4be3d4a6cbe95e08835f5fdcc2a6da0dc3
Rafal <kalawski1900@gmail.com>
2021-12-16 13:47:43 +0100

    fix bugs and add string functions for all models

--- final_project/e_drugs/admin.py
+++ final_project/e_drugs/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 
-from .models import Discount, Alert, Shipping, Substance, Affliction, MedicineInstance
-from .models import Discount, Alert, Shipping, Substance, Affliction, Medicine
+from .models import Discount, Alert, Shipping, Substance, Affliction, Medicine, MedicineInstance
 
-
-# Register your models here.
 admin.site.register(Alert)
 admin.site.register(Discount)
 admin.site.register(Shipping)
