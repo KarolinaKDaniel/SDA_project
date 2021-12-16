456bf50951f13e4a6800152141143faa6e7d89fc
Uzytkownik <uztkownik@kursGIT.pl>
2021-12-16 17:46:05 +0100

    add model Patient

--- final_project/accounts/models.py
+++ final_project/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models import CharField, Model, CASCADE, ImageField, OneToOneField, TextField, IntegerField,ForeignKey
+from django.db.models import CharField, Model, CASCADE, ImageField, OneToOneField, ManyToManyField
 from e_drugs.models import Affliction
 
 
