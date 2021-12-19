3ef637501579517c099845483894cd5f3eefe31e
Rafal <kalawski1900@gmail.com>
2021-12-19 14:49:15 +0100

    add medicine function with feature sort by name and price add feature search medicine by substance

--- final_project/e_drugs/urls.py
+++ final_project/e_drugs/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from .views import medicine, DrugDetailView, search_medicine
 
-urlpatterns = []
+urlpatterns = [
+    path('medicines', medicine, name='medicines-index'),
+    path('medicines/<int:pk>', DrugDetailView.as_view(), name='medicine-detail'),
+    path('search', search_medicine, name='search-medicine'),
+]
--- final_project/e_drugs/views.py
+++ final_project/e_drugs/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Medicine
 
-# Create your views here.
+def medicine(request):
+    sorting = request.GET.get('s', 'default')
+    if sorting == "name":
+        drugs_list = Medicine.objects.all().order_by("name")
+    elif sorting == "price_net":
+        drugs_list = Medicine.objects.all().order_by("price_net")
+    else:
+        drugs_list = Medicine.objects.all()
+    return render(request, template_name='medicines.html', context={'medicines': drugs_list})
+
+
+def search_medicine(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        medicines = Medicine.objects.filter(substance__name__contains=searched, substance__is_active=True)
+        return render(request, template_name='medicines.html',
+                      context={"searched": searched,
+                               "medicines": medicines})
+
+
+class DrugDetailView(DetailView):
+    template_name = 'medicine_detail.html'
+    model = Medicine
+    context_object_name = 'medicine'
