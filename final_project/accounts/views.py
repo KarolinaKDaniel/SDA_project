f5bee380ad29f03f899146df8a2fb7c9c6463a99
Uzytkownik <uztkownik@kursGIT.pl>
2021-12-19 20:41:40 +0100

    add view PatientDetailView

--- final_project/accounts/views.py
+++ final_project/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Patient
 
 
@@ -7,5 +7,11 @@ class PatientListView(ListView):
     template_name = 'patients.html'
     model = Patient
     context_object_name = 'patients'
+
+
+class PatientDetailView(DetailView):
+    model = Patient
+    template_name = 'patient_detail.html'
+    context_object_name = 'patient'
 
 # Create your views here.
