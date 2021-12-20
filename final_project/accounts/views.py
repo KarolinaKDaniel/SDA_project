e1bc9f1c7ee7b8a904e43ee2ce35cf78f20a6cf5
Uzytkownik <uztkownik@kursGIT.pl>
2021-12-20 18:41:11 +0100

    add get_context_data to the view PatientDetailView

--- final_project/accounts/views.py
+++ final_project/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Patient
+from e_drugs.models import Affliction
 
 
 class PatientListView(ListView):
@@ -13,5 +14,10 @@ class PatientDetailView(DetailView):
     model = Patient
     template_name = 'patient_detail.html'
     context_object_name = 'patient'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['patients'] = Affliction.objects.all()
+        return context
 
 # Create your views here.
