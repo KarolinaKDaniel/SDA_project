c4d7d583170a32785a867bcb3cb19baf684572ca
Ashegra <ashegra@gmail.com>
2021-12-22 22:00:13 +0100

    account models tweaks

--- final_project/accounts/models.py
+++ final_project/accounts/models.py
@@ -16,13 +16,29 @@ class Doctor(Model):
     credential_id = CharField(max_length=128)
     photo = ImageField(blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.my_user.last_name} {self.my_user.first_name}'
+
+    class Meta:
+        ordering = ['my_user__last_name']
 
 class Patient(Model):
     doctor = ManyToManyField(Doctor, blank=True)
     my_user = OneToOneField(MyUser, on_delete=CASCADE)
     affliction = ManyToManyField(Affliction, blank=True)
 
+    def __str__(self):
+        return f'{self.my_user.last_name} {self.my_user.first_name}'
+
+    class Meta:
+        ordering = ['my_user__last_name']
 
 class Pharmacist(Model):
     my_user = OneToOneField(MyUser, on_delete=CASCADE)
     credential_id = CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.my_user.last_name} {self.my_user.first_name}'
+
+    class Meta:
+        ordering = ['my_user__last_name']
