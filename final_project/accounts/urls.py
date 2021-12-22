d363fd643db741674624866da72360e9d41af92a
Rafal <kalawski1900@gmail.com>
2021-12-22 15:31:47 +0100

    add detailview for doctors

--- final_project/accounts/urls.py
+++ final_project/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import PatientListView, PatientDetailView, \
-    search_doctor, DoctorListView
+    search_doctor, DoctorListView, DoctorDetailView
 
 urlpatterns = [
     path('patients', PatientListView.as_view(), name='patients'),
     path('doctors', DoctorListView.as_view(), name='doctors-all'),
+    path('doctors/<int:pk>', DoctorDetailView.as_view(), name='doctor-detail'),
     path('search', search_doctor, name='search-doctor'),
     path('patients/<int:pk>', PatientDetailView.as_view(), name='patient-detail'),
 ]
