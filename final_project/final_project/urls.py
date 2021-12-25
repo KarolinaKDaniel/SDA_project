84872aa64f40c29062d462538a6db0f692de090a
Ashegra <ashegra@gmail.com>
2021-12-25 21:02:03 +0100

    base main_page view

--- final_project/final_project/urls.py
+++ final_project/final_project/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('e_drugs/', include('e_drugs.urls'))
+    path('', include('e_drugs.urls')),
 ]
